5ccc4845e4f042c660de9c0211b81fef74306ac2
Nofrets Poai <opetstudio@gmail.com>
2018-12-16 22:40:28 +0700

    dev employee service

--- src/main/java/com/prisma/entity/employee/controller/EmployeeController.java
+++ src/main/java/com/prisma/entity/employee/controller/EmployeeController.java
@@ -4,43 +4,53 @@ import java.util.List;
 import java.util.Optional;
 
 import com.prisma.entity.employee.dao.EmployeeRepository;
-import com.prisma.entity.employee.model.Employee;
+import com.prisma.entity.employee.model.TbEmployee;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/api/employee")
 public class EmployeeController {
  private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeController.class);
  
  @Autowired
  EmployeeRepository repository;
 
- @PostMapping("/")
- public Employee add(@RequestBody Employee employee) {
+ @PostMapping("/create")
+ public TbEmployee add(@RequestBody TbEmployee employee) {
     LOGGER.info("Employee add: {}", employee);
     return repository.save(employee);
  }
- @GetMapping("/{id}")
- public Optional<Employee> findById(@PathVariable("id") String id) {
+ @GetMapping("/read/{id}")
+ public Optional<TbEmployee> findById(@PathVariable("id") String id) {
   LOGGER.info("Employee find: id={}", id);
   return repository.findById(id);
+//   return repository.findByIdid)
  }
- @GetMapping("/department/{departmentId}")
- public List findByDepartment(@PathVariable("departmentId") Long departmentId) {
-  LOGGER.info("Employee find: departmentId={}", departmentId);
-  // return repository.findByDepartmentId(departmentId);
-  return repository.findByDepartmentId(departmentId);
+ @GetMapping("/read-all")
+ public Iterable<TbEmployee> readAll() {
+   Iterable<TbEmployee> all = repository.findAll(Sort.by("email"));
+   all.forEach(System.out::println);
+  return all;
  }
- @GetMapping("/organization/{organizationId}")
- public List findByOrganization(@PathVariable("organizationId") Long organizationId) {
-  LOGGER.info("Employee find: organizationId={}", organizationId);
-  return repository.findByOrganizationId(organizationId);
- }
+//  @GetMapping("/department/{departmentId}")
+//  public List findByDepartment(@PathVariable("departmentId") Long departmentId) {
+//   LOGGER.info("Employee find: departmentId={}", departmentId);
+//   // return repository.findByDepartmentId(departmentId);
+//   return repository.findByDepartmentId(departmentId);
+//  }
+//  @GetMapping("/organization/{organizationId}")
+//  public List findByOrganization(@PathVariable("organizationId") Long organizationId) {
+//   LOGGER.info("Employee find: organizationId={}", organizationId);
+//   return repository.findByOrganizationId(organizationId);
+//  }
 }
--- src/main/java/com/prisma/entity/employee/dao/EmployeeRepository.java
+++ src/main/java/com/prisma/entity/employee/dao/EmployeeRepository.java
@@ -1,20 +1,32 @@
 package com.prisma.entity.employee.dao;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
-import com.prisma.entity.employee.model.Employee;
+import com.prisma.entity.employee.model.TbEmployee;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
+// import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+// import org.springframework.data.mongodb.core.MongoTemplate;
+// import org.springframework.data.repository.CrudRepository;
+// import org.springframework.data.mongodb.repository.MongoRepository;
+// import org.springframework.stereotype.Repository;
 
 
-@Repository 
-public interface EmployeeRepository extends MongoRepository<Employee, String> {
+// @Repository
+@RepositoryRestResource(collectionResourceRel = "employee", path = "employee")
+public interface EmployeeRepository extends PagingAndSortingRepository<TbEmployee, String> {
     // { 'location' : { '$near' : [point.x, point.y], '$maxDistance' : distance}}
+    // @Override
+    // default Optional<TbEmployee> findById(String id) {
+    //     return null;
+    // }
     // List<Employee> findByLocationNear(Point location, Distance distance);
-    List<Employee> findByDepartmentId(Long departmentId);
-    List<Employee> findByOrganizationId(Long organizationId);
+    // List<Employee> findByDepartmentId(Long departmentId);
+    // List<Employee> findByOrganizationId(Long organizationId);
+    // List<Employee> findByCreatedbyOrderByCreatedonAsc(@Param("createdby") String createdby);
+    // List<Employee> findByCreatedbyOrderByCreatedonDesc(@Param("createdby") String createdby);
 }
